1107e71c877fbb5c9ce95b37642e45cca6e56b78
sighingpotato <jot0202@naver.com>
2026-05-08 09:48:22 +0900

    feat : 31번 문제 풀이

--- src/Solution31.java
+++ src/Solution31.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Solution31 {
+    public String solution31(int n) {
+        String answer = "";
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                answer += "수"; // 짝수 번째에는 "수"
+            } else {
+                answer += "박"; // 홀수 번째에는 "박"
+            }
+        }
+        return answer;
+    }
 }
